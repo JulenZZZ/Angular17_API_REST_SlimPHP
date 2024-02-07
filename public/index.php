321e6d8b18702224890b9a750c98810d147cd482
JulenZZZ <julenz485@gmail.com>
2024-02-07 20:33:00 +0100

    HTTP headers and CORS access granted

--- public/index.php
+++ public/index.php
@@ -10,6 +10,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+//Headers config
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 
 // Parse json, form data and xml
 $app->addBodyParsingMiddleware();
